27464b84fb3628b371a0f6089f2d08422934bf88
binhthanhnguyen <nguyen.thanh.binh@framgia.com>
2016-05-28 18:46:25 +0700

    Finish display table

--- app/src/main/java/com/example/binh/jsoupdemo/ui/fragments/TableFragment.java
+++ app/src/main/java/com/example/binh/jsoupdemo/ui/fragments/TableFragment.java
@@ -1,12 +1,22 @@
 package com.example.binh.jsoupdemo.ui.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.binh.jsoupdemo.R;
+import com.example.binh.jsoupdemo.data.models.Team;
+import com.example.binh.jsoupdemo.ui.adapters.TableAdapter;
+import com.example.binh.jsoupdemo.util.FootballFetcher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by binh on 5/18/16.
@@ -14,13 +24,47 @@ import com.example.binh.jsoupdemo.R;
 public class TableFragment extends Fragment {
     public static final String TAG = "TableFragment";
 
+    private RecyclerView mRecyclerView;
+    private List<Team> mTeams = new ArrayList<>();
     public static TableFragment newInstance() {
         TableFragment fragment = new TableFragment();
         return fragment;
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        new FetchTableTask().execute();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_table, container, false);
+        View view = inflater.inflate(R.layout.fragment_table, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        setupAdapter();
+        return view;
+    }
+
+    private class FetchTableTask extends AsyncTask<Void, Void, List<Team>>{
+
+        @Override
+        protected List<Team> doInBackground(Void... params) {
+            return new FootballFetcher().fetchTeamItems();
+        }
+
+        @Override
+        protected void onPostExecute(List<Team> teams) {
+            super.onPostExecute(teams);
+            mTeams = teams;
+            setupAdapter();
+        }
+    }
+
+    private void setupAdapter() {
+        if (isAdded()) {
+            mRecyclerView.setAdapter(new TableAdapter(mTeams));
+        }
     }
 }
--- app/src/main/java/com/example/binh/jsoupdemo/util/FootballFetcher.java
+++ app/src/main/java/com/example/binh/jsoupdemo/util/FootballFetcher.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.example.binh.jsoupdemo.data.models.Match;
 import com.example.binh.jsoupdemo.data.models.News;
+import com.example.binh.jsoupdemo.data.models.Team;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,6 +13,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -22,7 +24,7 @@ public class FootballFetcher {
 
     public static final String Match_Url = "http://tructiepbongda.com/full-match";
     public static final String News_Url = "http://bongdaplus.vn/tin-tuc/anh/ngoai-hang-anh/";
-
+    public static final String Table_Url = "http://www.24h.com.vn/bang-xep-hang-bong-da/bang-xep-hang-bong-da-anh-c295a466585.html";
 
     public List<Match> fetchRecentMatchItems() {
         List<Match> matches = new ArrayList<>();
@@ -34,6 +36,18 @@ public class FootballFetcher {
             Log.e(TAG, "Failed to fetch match items", e);
         }
         return matches;
+    }
+
+    public List<Team> fetchTeamItems() {
+        List<Team> teams = new ArrayList<>();
+        try {
+            Document document = Jsoup.connect(Table_Url).userAgent("Mozilla").get();
+            Elements elements = document.select("div.table-charts>div[id=zone_type_view_total]>div.table-responsive>div.table-one>table>tbody>tr");
+            parseTeamItems(teams, elements);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to fetch team items", e);
+        }
+        return teams;
     }
 
     public List<Match> searchMatchItems(String query) {
@@ -48,16 +62,6 @@ public class FootballFetcher {
         return matches;
     }
 
-    private void parseMatchItem(List<Match> matches, Elements elements) {
-        for (Element element : elements) {
-            String title = element.attr("title");
-            String href = element.attr("href");
-            String timestamp = element.parent().parent().select("div.time").text();
-            String imageUrl = element.select("img").attr("src");
-            matches.add(new Match(title, href, timestamp, imageUrl));
-        }
-    }
-
     public List<News> fetchNewsItems() {
         List<News> newses = new ArrayList<>();
         try {
@@ -67,7 +71,6 @@ public class FootballFetcher {
             Log.e(TAG, "Failed to parse News item", e);
         }
         return newses;
-
     }
 
     private void parseNewsItem(List<News> newses, Document document) {
@@ -80,6 +83,35 @@ public class FootballFetcher {
             String desc = element.parent().ownText();
             String imageUrl =  element.select("img").attr("src");
             newses.add(new News(title,desc,imageUrl, href, "23/4/2016"));
+        }
+    }
+
+    private void parseMatchItem(List<Match> matches, Elements elements) {
+        for (Element element : elements) {
+            String title = element.attr("title");
+            String href = element.attr("href");
+            String timestamp = element.parent().parent().select("div.time").text();
+            String imageUrl = element.select("img").attr("src");
+            matches.add(new Match(title, href, timestamp, imageUrl));
+        }
+    }
+
+    private void parseTeamItems(List<Team> teams,Elements elements) {
+        int position,gp,wins, draws, losses, pts;
+        String imageUrl, name;
+
+        for(Element tr : elements) {
+            Iterator<Element> elementIterator =  tr.select("td").iterator();
+            Elements tds = tr.select("td");
+            position = Integer.parseInt(tds.get(0).text());
+            imageUrl = tds.get(1).select("div.bxh-anh-doi-bong-da>img.logo-bxh").attr("src");
+            name = tds.get(1).text();
+            gp = Integer.parseInt(tds.get(2).text());
+            wins = Integer.parseInt(tds.get(3).text());
+            draws = Integer.parseInt(tds.get(4).text());
+            losses = Integer.parseInt(tds.get(5).text());
+            pts = (Integer.parseInt(tds.get(9).text()));
+            teams.add(new Team(position, name, imageUrl, pts,gp, wins, draws, losses));
         }
     }
 
